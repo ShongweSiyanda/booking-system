a0df17ceecf74726a61112bfc23bbb7bf0666be0
siyanda-shongwe <shongwesiyanda@gmail.com>
2018-11-21 12:52:28 +0200

    session file added

---
+++ php_scripts/session.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require 'connect.php';
+session_start();
+
+$userCheck = $_SESSION["login"];
+$sess_sql = "select username from users where username='$userCheck'";
+$results = mysqli_query($conn, $sess_sql);
+$row = mysqli_fetch_array($results,MYSQLI_ASSOC);
+$login_user_session = $row["username"];
+
+if(!isset($_SESSION["login"])){
+echo("<script>
+    window.open(...);
+    window.alert('Session expired, please login again');
+</script>");
+    header("location:index.html");
+}
+
+
+?>
